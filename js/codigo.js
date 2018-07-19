e6e479fa053e3fed63476c2de11763d9c7d13514
Roman, Pablo <pabloro_lu@lagash.com>
2018-07-19 12:57:52 -0300

    pagina web

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+$(document).on('ready', function () {
+	$('#boton-menu').on('click', function () {
+		$('#menu-ul').slideToggle('fast');
+		$('#menu').css('border-bottom', 'none');
+		$('#menu').css('box-shadow', 'none');
+
+	});
+	if ($('html').hasClass('desktop')) {
+		new WOW().init();
+	}
+	wow = new WOW({
+		animateClass: 'animated',
+		offset: 100
+	});
+	wow.init();
+	document.getElementById('moar').onclick = function () {
+		var section = document.createElement('section');
+		section.className = 'section--purple wow fadeInDown';
+		this.parentNode.insertBefore(section, this);
+	};
+});
